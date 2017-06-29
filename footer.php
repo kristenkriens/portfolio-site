45b94a28f8b3cb144c48015a5ed5194af5a4e5bf
Kristen Kriens <kristen.kriens@gmail.com>
2017-06-29 16:19:19 -0400

    Make social links in footer into list and make all associated changes

--- footer.php
+++ footer.php
@@ -3,22 +3,22 @@
   	 <div class="footer-text">
     	<p>&copy; Kristen Kriens <?php echo date('Y'); ?>. <?php the_field('footer_text', 'options'); ?></p>
     </div>
-    <div class="footer-links">
+    <ul class="footer-links">
 	    <?php
 	      if(have_rows('social_links', 'options')) {
 	        while(have_rows('social_links', 'options')) {
 	          the_row();
 	          ?>
-	          <a href="<?php the_sub_field('social_link_url', 'options'); ?>" title="<?php the_sub_field('social_link_name', 'options'); ?>" target="_blank">
-	            <div class="footer-link">
-	              <i class="fa fa-<?php the_sub_field('social_link_name', 'options'); ?>" aria-hidden="true"></i>
-	            </div>
-	          </a>
+	          <li class="footer-link">
+              <a href="<?php the_sub_field('social_link_url', 'options'); ?>" title="<?php the_sub_field('social_link_name', 'options'); ?>" target="_blank">
+  	             <i class="fa fa-<?php the_sub_field('social_link_name', 'options'); ?>" aria-hidden="true"></i>
+  	          </a>
+            </li>
 	          <?php
 	        } // end while
 	      } // end if
 	    ?>
-	 </div>
+	 </ul>
   </div>
 </footer>
 
