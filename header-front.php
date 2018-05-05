5929db66257efcb97e69bb25171108e29225e70d
Kristen Kriens <kristen.kriens@gmail.com>
2018-05-05 15:11:01 -0400

    Update GA scripts

--- header-front.php
+++ header-front.php
@@ -8,6 +8,14 @@
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
   <!-- stylesheets should be enqueued in functions.php -->
   <?php wp_head(); ?>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-71145610-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-71145610-1');
+  </script>
 </head>
 
 <body <?php body_class(); ?>>
